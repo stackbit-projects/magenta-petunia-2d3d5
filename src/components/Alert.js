601b4f41583d734c5ce03448ebd16bbfacd01b54
Stackbit Code Editor <projects@stackbit.com>
2021-11-04 18:45:31 +0000

    Alert.js: changed

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,5 +1,7 @@
-const Alert = ({body}) => {
+const Alert = ({ body }) => {
     return <div className="p-4 bg-primary text-white text-center">
-        <div className="max-w-md"
+        <div className="max-w-lg mx-auto">
+            <p>{body}</p>
+        </div>
     </div>
 }
